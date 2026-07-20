fb54fbefa810166320d2c44a256d6e8532ebaf3b
UJayathilaka <udajayathilaka21@gmail.com>
2026-07-20 22:32:45 +0530

    Update gxs-uploader.user.js

--- gxs-uploader.user.js
+++ gxs-uploader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Image Mapping + Batch Upload & Row Opener (UX Optimized)
 // @namespace    http://tampermonkey.net/
-// @version      8.5.0
+// @version      8.5.1
 // @description  Fully English translated, fixed bottom UI, Auto Sort, Instant Stop, Strict Dropdown Selection, Instant Image Upload Verification & Safe Saving
 // @author       UJay
 
